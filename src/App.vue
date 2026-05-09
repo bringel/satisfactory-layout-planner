9b7c15c10e3932aaf412f9e6764309321a875a86
Brad Ringel <brad.ringel@gmail.com>
2026-05-09 14:27:25 -0400

    Adding extremely basic layout shell

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <nav class="px-4 py-4 bg-gray-800 h-16 flex-none">
+    <h1 class="text-2xl text-white font-bold">Satisfactory Layout Planner</h1>
+  </nav>
+  <div class="grid grid-cols-[3fr_1fr] grow bg-gray-200">
+    <svg></svg>
+    <div class="border-l border-gray-700"></div>
+  </div>
+</template>
 
 <style scoped></style>
